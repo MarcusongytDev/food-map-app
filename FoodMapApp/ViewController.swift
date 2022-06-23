c0618824aae7874592e3d1f891ca1e86eaa4e41a
Marcus Ong <marcusongyt@gmail.com>
2022-06-23 14:37:33 +0800

    Initial Commit

---
+++ FoodMapApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodMapApp
+//
+//  Created by Marcus Ong on 23/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
